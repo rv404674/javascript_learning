57864aa25224aac2d152737a7d520ed1c181246c
Rahul Verma <rv404674@gmail.com>
2023-01-07 13:50:35 +0530

    Learnt about JS functions and data type

---
+++ dataTypes.js
@@ -0,0 +1,29 @@
+// numbers
+let length = 16;
+let weight = 100;
+
+// Strings
+let color = "Yellow";
+let lastName = "Johnson";
+
+// booleans
+let x = true;
+let y = false;
+
+// Object
+const person = {firstName:"John", lastName:"Rahul"};
+
+// Array object
+let cars = ["sfs", "sdfs"];
+
+// in js single and double quote are the same
+let answer1 = "I am correct";
+let answer2 = 'I am correct';
+
+// NOTE: typeOf returns the type of variable
+typeof 0
+typeof 314
+typeof 3.14
+
+// By default value is undefined.
+let car; // Value is undefined, type is undefined.
---
+++ functions.js
@@ -0,0 +1,19 @@
+// a js function is called when something invokes it.
+// 1. when an event occurs (when a user clicks on the button).
+// 2. when it is invoked (called) from JS code
+// 3. Automatically (self invoked)
+
+
+function myFunction(p1, p2){
+    return p1 * p2;
+}
+
+let x = myFunction(4,3);
+console.log(x);
+
+function toCelsius(fahrenheit){
+    return (5/9) * (fahrenheit-32);
+}
+
+let x = toCelsius(77);
+console.log(x);
