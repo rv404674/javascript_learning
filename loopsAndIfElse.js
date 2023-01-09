71e32bec61ff535b483fde556f1fc1b424a80d4a
Rahul Verma <rv404674@gmail.com>
2023-01-09 15:01:35 +0530

    learning about if else

---
+++ loopsAndIfElse.js
@@ -0,0 +1,10 @@
+// conditional operator
+let age = 20;
+let voteable = (age < 18) ? "too young" : "old enough";
+
+// NOTE: JS will compare string and int.
+// x will be true.
+// A nonumeric string will be converted to NaN, which is always false.
+let x = 2 < "12"
+let y = 2 > "John"
+
