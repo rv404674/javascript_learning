bfbcf6f50cd81f39599bab83b1cb930593ee4152
Rahul Verma <rv404674@gmail.com>
2023-01-10 13:25:31 +0530

    done with loops

--- loopsAndIfElse.js
+++ loopsAndIfElse.js
@@ -8,3 +8,34 @@ let voteable = (age < 18) ? "too young" : "old enough";
 let x = 2 < "12"
 let y = 2 > "John"
 
+const dogObject = {
+    dogName : "rubu",
+    dogAge: 9
+}
+
+if (dogObject.dogAge > 10){
+    console.log("budha doggu")
+} else if (dogObject.dogAge > 5) {
+    console.log("mid life dog")
+} else {
+    console.log("young dog");
+}
+
+let day = 1;
+switch (day){
+    case 0 :
+        day = "Synday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 4:
+        day = "wednesday"
+        break;
+    default:
+        text = "looking forward to ehe weekend."
+}
+
